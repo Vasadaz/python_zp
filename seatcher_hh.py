0ccb88496254724ee3bb8a1935f46dfc2a41a4f0
Nikolai Sysoev <81381305+Vasadaz@users.noreply.github.com>
2022-06-09 17:22:28 +0300

    Add new request

--- seatcher_hh.py
+++ seatcher_hh.py
@@ -1,12 +1,34 @@
 import requests
+import json
 
 
 url = 'https://api.hh.ru/vacancies/'
-payload = {'text': 'Программист'}
+payload_all = {'text': 'Программист',
+           'area': '1',
+           'currency': 'RUR',
+           'only_with_salary': 'true'}
 
-response = requests.get(url, params=payload)
-response.raise_for_status()
+payload_one_month = {'text': 'Программист',
+           'area': '1',
+           'currency': 'RUR',
+           'only_with_salary': 'true',
+            'period': 30}
 
-imgs = response.json()
+response_all = requests.get(url, params=payload_all)
+response_all.raise_for_status()
+all_data = response_all.json()
 
-print(imgs)
+response_one_month = requests.get(url, params=payload_one_month)
+response_one_month.raise_for_status()
+one_month_data = response_one_month.json()
+
+for vacant in all_data['items']:
+    print(vacant['name'])
+    print(vacant['area']['name'])
+    print('ЗП от', vacant['salary']['from'], vacant['salary']['currency'])
+    print(vacant['published_at'])
+    print()
+
+
+print('Вакансий за всё время:', all_data['found'])
+print('Вакансий за последний месяц:', one_month_data['found'])
