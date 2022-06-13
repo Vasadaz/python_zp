9c23e6678c9d11a34f2b954b70a8b81c325ca66a
Nikolai Sysoev <81381305+Vasadaz@users.noreply.github.com>
2022-06-13 13:25:03 +0300

    Updated the "search_vacancies" and "predict_rub_salary" functions to work with all search pages.

--- seatcher_hh.py
+++ seatcher_hh.py
@@ -1,4 +1,5 @@
 import statistics as stat
+from itertools import count
 
 import requests
 
@@ -7,25 +8,36 @@ PROG_LANGS = ['JavaScript', 'Java', 'Python', 'Ruby', 'PHP', 'C++', 'C', 'C#',
               'Go', 'Shell', 'Objective-C', 'Scala', 'Swift', 'TypeScript']
 
 
-def searh_vacancies(languages: list) -> dict:
+def search_vacancies(languages: list) -> dict:
     popular_prog_langs = {}
+    vacancies_found = None
 
     for lang in languages:
+        all_vacansies = {}
 
-        url = 'https://api.hh.ru/vacancies/'
-        payload_all = {'text': f'Программист {lang}',
-                       'area': '1',
-                       "per_page": 100,
-                       'currency': 'RUR',
-                       'only_with_salary': 'true'}
+        for page in count(0):
+            url = 'https://api.hh.ru/vacancies/'
+            payload_all = {'text': f'Программист {lang}',
+                           'area': '2',
+                           'page': page,
+                           'per_page': 100,
+                           'currency': 'RUR',
+                           'only_with_salary': 'true'}
 
-        response = requests.get(url, params=payload_all)
-        response.raise_for_status()
-        response_data = response.json()
-        vacancies_found = int(response_data['found'])
-        vacancies_processed, average_salary = predict_rub_salary(response_data['items'])
+            page_response = requests.get(url, params=payload_all)
+            page_response.raise_for_status()
+            page_data = page_response.json()
+            vacancies_found = int(page_data['found'])
+            num_pages = page_data['pages']
 
-        if vacancies_found > 100:
+            if page == num_pages or vacancies_found < 100:
+                break
+            else:
+                all_vacansies[page] = (page_data['items'])
+
+        if all_vacansies:
+            vacancies_processed, average_salary = predict_rub_salary(all_vacansies)
+
             popular_prog_langs[lang] = {'vacancies_found': vacancies_found,
                                         'vacancies_processed': vacancies_processed,
                                         'average_salary': average_salary}
@@ -35,18 +47,19 @@ def searh_vacancies(languages: list) -> dict:
 def predict_rub_salary(vacansies: dict) -> list:
     salaries_avg = []
 
-    for vacancy in vacansies:
-        salary_from = vacancy['salary']['from']
-        salary_to = vacancy['salary']['to']
+    for page in vacansies:
+        for vacancy in vacansies[page]:
+            salary_from = vacancy['salary']['from']
+            salary_to = vacancy['salary']['to']
 
-        if salary_from is None and salary_to is None:
-            continue
-        elif salary_from is None:
-            salaries_avg.append(salary_to * 0.8)
-        elif salary_to is None:
-            salaries_avg.append(salary_from * 1.2)
-        else:
-            salaries_avg.append(stat.mean([salary_from, salary_to]))
+            if salary_from is None and salary_to is None:
+                continue
+            elif salary_from is None:
+                salaries_avg.append(salary_to * 0.8)
+            elif salary_to is None:
+                salaries_avg.append(salary_from * 1.2)
+            else:
+                salaries_avg.append(stat.mean([salary_from, salary_to]))
 
     salaries_avg_number = len(salaries_avg)
     salary_avg = int(stat.mean(salaries_avg))
@@ -55,7 +68,8 @@ def predict_rub_salary(vacansies: dict) -> list:
 
 
 if __name__ == '__main__':
-    popular_langs = searh_vacancies(PROG_LANGS)
+    popular_langs = search_vacancies(PROG_LANGS)
+
     for i in popular_langs:
         print(i)
         for j in popular_langs[i].items():
