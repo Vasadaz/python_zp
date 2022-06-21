54271c8693115d8322af6635d90e04efa24808d6
Nikolai Sysoev <81381305+Vasadaz@users.noreply.github.com>
2022-06-21 16:46:58 +0300

    Reformatted code.

--- mean_salary_programmer.py
+++ mean_salary_programmer.py
@@ -7,11 +7,10 @@ import requests
 from dotenv import load_dotenv
 from terminaltables import AsciiTable
 
-
 HH_MIN_LIMIT = 100
 HH_POPULAR_PROG_LANGS = [['Язык программирования', 'Вакансий найдено',
                           'Вакансий обработано', 'Средняя зарплата']]
-HH_VACANCIES_FOUND = 0
+
 
 PROG_LANGS = ['JavaScript', 'Java', 'Python', 'Ruby', 'PHP', 'C++', 'C', 'C#',
               'Go', 'Shell', 'Objective-C', 'Scala', 'Swift', 'TypeScript']
@@ -19,30 +18,29 @@ PROG_LANGS = ['JavaScript', 'Java', 'Python', 'Ruby', 'PHP', 'C++', 'C', 'C#',
 SJ_MIN_LIMIT = 20
 SJ_POPULAR_PROG_LANGS = [['Язык программирования', 'Вакансий найдено',
                           'Вакансий обработано', 'Средняя зарплата']]
-SJ_VACANCIES_FOUND = 0
 
 
 def search_vacancies_hh(language: str) -> dict:
-    global HH_MIN_LIMIT, HH_VACANCIES_FOUND
+    global HH_MIN_LIMIT, hh_vacancies_found
 
     vacansies = {}
 
     for page in count(0):
         url = 'https://api.hh.ru/vacancies/'
-        payload_all = {'text': f'Программист {language}',
-                       'area': '2',
-                       'page': page,
-                       'per_page': 100,
-                       'currency': 'RUR',
-                       'only_with_salary': 'true'}
+        payload = {'text': f'Программист {language}',
+                   'area': '2',
+                   'page': page,
+                   'per_page': 100,
+                   'currency': 'RUR',
+                   'only_with_salary': 'true'}
 
-        page_response = requests.get(url, params=payload_all)
+        page_response = requests.get(url, params=payload)
         page_response.raise_for_status()
         page_data = page_response.json()
-        HH_VACANCIES_FOUND = int(page_data['found'])
+        hh_vacancies_found = int(page_data['found'])
         num_pages = page_data['pages']
 
-        if page == num_pages or HH_VACANCIES_FOUND < HH_MIN_LIMIT:
+        if page != num_pages or hh_vacancies_found > HH_MIN_LIMIT:
             break
         else:
             vacansies[page] = (page_data['items'])
@@ -51,7 +49,7 @@ def search_vacancies_hh(language: str) -> dict:
 
 
 def search_vacancies_sj(language: str, token: str) -> dict:
-    global SJ_MIN_LIMIT, SJ_VACANCIES_FOUND
+    global SJ_MIN_LIMIT, sj_vacancies_found
 
     vacansies = {}
 
@@ -66,9 +64,9 @@ def search_vacancies_sj(language: str, token: str) -> dict:
         page_response = requests.get(url, headers=head, params=payload)
         page_response.raise_for_status()
         page_data = page_response.json()
-        SJ_VACANCIES_FOUND = int(page_data['total'])
+        sj_vacancies_found = int(page_data['total'])
 
-        if page == 5 or SJ_VACANCIES_FOUND < SJ_MIN_LIMIT:
+        if page == 5 or sj_vacancies_found < SJ_MIN_LIMIT:
             break
         else:
             vacansies[page] = (page_data['objects'])
@@ -80,7 +78,7 @@ def predict_rub_salary(vacansies: dict,
                        keyword_salary=None,
                        keyword_from=None,
                        keyword_to=None) -> list:
-    salaries_avg = []
+    avg_salaries = []
 
     for page in vacansies:
         for vacancy in vacansies[page]:
@@ -94,16 +92,16 @@ def predict_rub_salary(vacansies: dict,
             if not salary_from and not salary_to:
                 continue
             elif not salary_from:
-                salaries_avg.append(salary_to * 0.8)
+                avg_salaries.append(salary_to * 0.8)
             elif not salary_to:
-                salaries_avg.append(salary_from * 1.2)
+                avg_salaries.append(salary_from * 1.2)
             else:
-                salaries_avg.append(stat.mean([salary_from, salary_to]))
+                avg_salaries.append(stat.mean([salary_from, salary_to]))
 
-    salaries_avg_number = len(salaries_avg)
-    salary_avg = int(stat.mean(salaries_avg))
+    avg_salaries_number = len(avg_salaries)
+    avg_salary = int(stat.mean(avg_salaries))
 
-    return [salaries_avg_number, salary_avg]
+    return [avg_salaries_number, avg_salary]
 
 
 def make_table(title: str, table_data: list) -> str:
@@ -117,31 +115,34 @@ if __name__ == '__main__':
     load_dotenv(dotenv_path)
     sj_token = os.environ["SJ_TOKEN"]
 
-    for lang in PROG_LANGS:
-        hh_vacansies = search_vacancies_hh(lang)
-        sj_vacansies = search_vacancies_sj(lang, sj_token)
+    hh_vacancies_found = 0
+    sj_vacancies_found = 0
 
-        if hh_vacansies:
-            hh_avg_salary = predict_rub_salary(hh_vacansies,
+    for lang in PROG_LANGS:
+        hh_vacanсies = search_vacancies_hh(lang)
+        sj_vacanсies = search_vacancies_sj(lang, sj_token)
+
+        if hh_vacanсies:
+            hh_avg_salary = predict_rub_salary(hh_vacanсies,
                                                keyword_salary='salary',
                                                keyword_from='from',
                                                keyword_to='to')
             hh_vacancies_processed = hh_avg_salary[0]
             hh_average_salary = hh_avg_salary[1]
             HH_POPULAR_PROG_LANGS.append([lang,
-                                          HH_VACANCIES_FOUND,
+                                          hh_vacancies_found,
                                           hh_vacancies_processed,
                                           hh_average_salary])
 
-        if sj_vacansies:
-            sj_avg_salary = predict_rub_salary(hh_vacansies,
+        if sj_vacanсies:
+            sj_avg_salary = predict_rub_salary(hh_vacanсies,
                                                keyword_salary='salary',
                                                keyword_from='payment_from',
                                                keyword_to='payment_to')
             sj_vacancies_processed = sj_avg_salary[0]
             sj_average_salary = sj_avg_salary[1]
             SJ_POPULAR_PROG_LANGS.append([lang,
-                                          SJ_VACANCIES_FOUND,
+                                          sj_vacancies_found,
                                           sj_vacancies_processed,
                                           sj_average_salary])
 
